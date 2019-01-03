10d4cb3d12ea2834a32975e86fd48d5e2a344bcc
roroku02 <roroku02@gmail.com>
2019-01-04 00:34:58 +0900

    アニメタグ検索2019年追加

--- main.php
+++ main.php
@@ -112,6 +112,7 @@ $tweet = "";
             <option value="2016">2016</option>
             <option value="2017">2017</option>
             <option value="2018">2018</option>
+            <option value="2019">2019</option>
         </select>
         <select name="season">
             <option value="1">冬</option>
